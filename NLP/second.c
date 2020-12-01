8db3c7a79833b2df1678df5cdc2d7adfbaefaacc
Hiroki-Noguchi0001 <s19C1091DK@s.chibakoudai.jp>
2020-12-02 05:15:01 +0900

    finish second

--- NLP/second.c
+++ NLP/second.c
@@ -5,60 +5,66 @@ int main(void){
 	FILE *Outfp;
 	char *output_file = "output.txt";
 	char small_alpha[26];
-	int counter[26] = {};
-	int count = 0;
-	char a = 97;// 97 = a
+	int count[27] = {};
+	int tmp1, tmp2;
+	char a = 0x61;
 	char mozi;
 
 	Outfp = fopen(output_file, "r");
-
+	
 	if(Outfp == NULL)
 	{
 		printf("%sのオープンに失敗しました.\n", output_file);
 		return 1;
 	}
 
-
 	for(int i = 0; i < 26; i++)
 	{
 		small_alpha[i] = a;
 		a++;
 	}
-	small_alpha[26] = 32;//32 = ' '
 
-	while(mozi = fgetc(Outfp) != EOF)
+	small_alpha[26] = 0x20;
+
+	while((mozi = fgetc(Outfp)) != EOF)
 	{
+		mozi = tolower(mozi);
 
-		printf("%c", mozi);
-
-
-		/*
-
-		if(mozi == small_alpha[26])
+		if(mozi == 0x20)
 		{
-			count++;
+			count[26]++;
 		}
 
-		else
+		if(mozi >= 0x61 && mozi <= 0x7A)
 		{
-			for(int i = 0; i < 26; i++)
-			{
-				if(tolower(mozi) == small_alpha[i])
-				{
-					counter[i] = 1; 
-				}
-			}
+			count[(int)mozi - 0x61]++;
 		}
-
-		*/
 	}
+
+	for (int i=0; i<27; ++i)
+	{
+    	for (int j=i+1; j<27; ++j)
+		{
+      		if (count[i] < count[j]) 
+			{
+        		tmp1 = count[i];
+        		tmp2 = small_alpha[i];
+        		count[i] = count[j];
+				small_alpha [i]= small_alpha[j];
+        		count[j] = tmp1;
+        		small_alpha[j] = tmp2;
+      		}
+    	}
+  }
 
 	for(int i = 0; i <= 26; i++)
 	{
-		printf("%c = %d\n", small_alpha[i], counter[i]);
+		printf("%c = %d\n", small_alpha[i], count[i]);
 	}
 
 	fclose(Outfp);
+
+	printf("上記の文字の出現頻度は大文字・小文字の区別はしていません。\n");
 
 	return 0;
 }
--- NLP/test.c
+++ NLP/test.c
@@ -1,26 +1,57 @@
 #include <stdio.h>
-#include <ctype.h>
 
-int main(void) {
-    FILE *Outfp;
-	char *output_file = "output.txt";
-    char mozi;
+int main(){
+  int i, j, tmp0, tmp1, tmp2, alpha[27] = {};
+  int num = 0;
+  FILE *fp;
+  char str;
+  char low[27], up[27];
+  char a = 0x61, A = 0x41;
+  char output_text[81] = {"output.txt"};
 
-	Outfp = fopen(output_file, "r");
-	
-	if(Outfp == NULL)
-	{
-		printf("%sのオープンに失敗しました.\n", output_file);
-		return 1;
-	}
+  fp = fopen(output_text, "r");
 
-    while (( mozi = fgetc(Outfp)) != EOF )
-	{
-		printf("%c", mozi);
+  if(fp == NULL){
+    printf("file open error\n");
+    return -1;
+  }
+
+  for(i = 0;i < 26;i++){
+    low[i] = a;
+    up[i] = A;
+    a++;
+    A++;
+  }
+  low[26] = 0x20;
+  up[26] = 0x20; 
+    
+
+  while((str = fgetc(fp)) != EOF){
+    if(str >= 0x61 && str <= 0x7A){
+      alpha[(int)str - 0x61]++;
+      num++;
     }
+    if(str >= 0x41 && str <= 0x5A){
+      alpha[(int)str - 0x41]++;
+      num++;
+    }
+    if(str == 0x20){
+      alpha[26]++;
+      num++;
+    }
+      i++;
+  }
 
-	fclose(Outfp);
-	printf("ファイルの読み込みが成功しました\n");
-    return 0;
+
+  printf("Frequency of appearance\n");
+
+  for(i = 0;i < 27;i++){
+    printf("[No.%d] %c,%c :%d\n", i+1, low[i], up[i], alpha[i]);
+  }
+  
+  printf("Total number of characters: %d\n", num);
+
+  fclose(fp);
+  
+  return 0;
 }
-
